5fe011e411c9f81e06b4440fc890ca5e3d90ba21
immunity428 <pega921sus@gmail.com>
2025-04-01 21:25:30 +0900

    docs:環境構築

---
+++ src/app/component/Atos/Title/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const Title = () => {
+  return <div className='text-black'>index </div>;
+};
+export default Title;
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Title from './component/Atos/Title/index';
+
+export default function Home() {
+  return (
+    <>
+      <div className='text-white'>Hello</div>
+      <Title></Title>
+    </>
+  );
+}
