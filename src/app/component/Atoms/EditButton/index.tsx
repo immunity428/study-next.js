e03abc13d2f9daca5547d54d43d9040d5de64208
immunity428 <pega921sus@gmail.com>
2025-04-05 21:14:23 +0900

    ブランチ移動のため一旦コミット

--- src/app/component/Atoms/EditButton/index.tsx
+++ src/app/component/Atoms/EditButton/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 
-import pencil from '../../../../assets/svg/pencil.svg';
+const Pencil = dynamic(
+  () => import('../../../../../public/assets/svg/pencil.svg')
+);
 
 const EditButton = () => {
   return (
-    <div className=''>
-      <img src={pencil} alt='EditButtton' className='w-4 h-4' />
+    <div className='hover:bg-[#B8B8B8] hover:cursor-pointer rounded-full w-5 h-5 opacity-80'>
+      <Pencil className='w-full h-full object-cover alt="EditButton' />
     </div>
   );
 };
+
 export default EditButton;
