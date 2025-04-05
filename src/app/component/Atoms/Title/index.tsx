4e532781436c7ab8e4f456a21a8ae45ff1e81e5f
immunity428 <pega921sus@gmail.com>
2025-04-05 21:28:25 +0900

    fix:コメント内容の修正

--- src/app/component/Atoms/Title/index.tsx
+++ src/app/component/Atoms/Title/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 const Title = () => {
   return (
-    <div className='min-h-screen bg-[#2F3136]'>
-      <h1 className='text-white text-2xl leading-9 md:text-4xl md:leading-13 font-roboto'>
-        SIMPLE TODO APP
-      </h1>
-    </div>
+    <h1 className='text-white text-2xl leading-9 md:text-4xl md:leading-13 font-roboto font-normal'>
+      SIMPLE TODO APP
+    </h1>
   );
 };
 export default Title;
