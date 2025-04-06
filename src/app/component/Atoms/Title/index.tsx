c523f68697d6ccdcc997435a5113457f998a56d8
immunity428 <pega921sus@gmail.com>
2025-04-06 12:59:12 +0900

    fix:font-weightの変更

--- src/app/component/Atoms/Title/index.tsx
+++ src/app/component/Atoms/Title/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const Title = () => {
   return (
-    <h1 className='text-white text-2xl leading-9 md:text-4xl md:leading-13 font-roboto font-normal'>
+    <h1 className='text-white text-2xl leading-9 md:text-4xl md:leading-13 font-roboto font-medium'>
       SIMPLE TODO APP
     </h1>
   );
