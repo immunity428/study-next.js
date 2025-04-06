dd1aacc94b48e2e9e5949d01da94d5d6c770f13b
immunity428 <pega921sus@gmail.com>
2025-04-06 15:51:53 +0900

    feat:AddTaskButtonの実装

---
+++ src/app/component/Atoms/AddTaskButton/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+const Plus = dynamic(() => import('../../../../../public/assets/svg/Plus.svg'));
+
+const EditButton = () => {
+  return (
+    <div className='relative w-35 h-8 flex justify-center items-center'>
+      <div className='flex flex-row items-center justify-center gap-[10px] px-[2px] py-[6px]'>
+        <Plus className='z-0' />
+        <span className='text-[#46A381] font-[Noto_Sans_JP] text-sm not-italic font-medium leading-5'>
+          タスクを追加
+        </span>
+      </div>
+      <div className='absolute inset-0 hover:bg-[#46A381] opacity-20 rounded-full z-1' />
+    </div>
+  );
+};
+
+export default EditButton;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Title from './component/Atoms/Title/index';
 import EditButton from './component/Atoms/EditButton/index';
+import AddTaskButton from './component/Atoms/AddTaskButton/index';
 
 export default function Home() {
   return (
     <>
       <Title />
       <EditButton />
+      <AddTaskButton />
     </>
   );
 }
