14443948fc5aa3d9f9832a8d17cf37c9aa6d8e59
immunity428 <pega921sus@gmail.com>
2025-04-08 22:33:52 +0900

    fix:不要なcssの削除

--- src/app/component/Atoms/EditButton/index.tsx
+++ src/app/component/Atoms/EditButton/index.tsx
@@ -4,7 +4,7 @@ import Pencil from '../../../../../public/pencil.svg';
 const EditButton = () => {
   return (
     <div className='w-5 h-5 flex justify-center items-center'>
-      <Pencil className='z-0 hover:bg-[#B8B8B8]/20 rounded-full' />
+      <Pencil className='hover:bg-[#B8B8B8]/20 rounded-full' />
     </div>
   );
 };
