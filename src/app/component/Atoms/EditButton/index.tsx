a784a8a58f5fdcdbfec1707932c4b1a94e05efe1
immunity428 <pega921sus@gmail.com>
2025-04-08 21:55:16 +0900

    fix:cssの最適化

--- src/app/component/Atoms/EditButton/index.tsx
+++ src/app/component/Atoms/EditButton/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
-
-const Pencil = dynamic(
-  () => import('../../../../../public/assets/svg/pencil.svg')
-);
+import Pencil from '../../../../../public/pencil.svg';
 
 const EditButton = () => {
   return (
-    <div className='relative w-5 h-5 flex justify-center items-center'>
-      <Pencil className='z-0' />
-      <div className='absolute inset-0 hover:bg-[#B8B8B8] opacity-20 rounded-full z-1' />
+    <div className='w-5 h-5 flex justify-center items-center'>
+      <Pencil className='z-0 hover:bg-[#B8B8B8]/20 rounded-full' />
     </div>
   );
 };
