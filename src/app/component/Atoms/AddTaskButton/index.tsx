9bb67a3ceccab43b48e684085ed3e23f61902671
immunity428 <pega921sus@gmail.com>
2025-04-08 22:40:13 +0900

    fix:不要なcssの削除

--- src/app/component/Atoms/AddTaskButton/index.tsx
+++ src/app/component/Atoms/AddTaskButton/index.tsx
@@ -4,7 +4,7 @@ import Plus from '../../../../../public/plus.svg';
 const AddTaskButton = () => {
   return (
     <div className='relative w-35 h-8 flex justify-center items-center flex-row gap-[10px] px-[2px] py-[6px] hover:bg-[#46A381]/20 rounded-full'>
-      <Plus className='z-0' />
+      <Plus />
       <span className='text-[#46A381] font-[Noto_Sans_JP] text-sm not-italic font-medium leading-5'>
         タスクを追加
       </span>
