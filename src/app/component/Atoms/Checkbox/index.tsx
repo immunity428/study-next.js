afaed32ac9ae370b32d14fcbf2963f23deda72b3
immunity428 <pega921sus@gmail.com>
2025-04-08 23:13:37 +0900

    あきらめて聞くことにする

--- src/app/component/Atoms/Checkbox/index.tsx
+++ src/app/component/Atoms/Checkbox/index.tsx
@@ -4,7 +4,9 @@ import Check from '../../../../../public/check.svg';
 const Checkbox = () => {
   return (
     <div className='w-5 h-5 border-[2px] border-[#B8B8B8] flex items-center justify-center group'>
-      <Check className='hidden group-hover:block ' />
+      <Check className='hidden group-hover:block' />
+
+      {/* このコードだと✅が正方形の中央に来ないので、checkのサイズを指定するとhoverがつかえなくなる。 */}
     </div>
   );
 };
