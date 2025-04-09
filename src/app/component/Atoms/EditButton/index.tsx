741a48697a898e13f8a6b03b3db08fb33597c4d3
immunity428 <pega921sus@gmail.com>
2025-04-09 19:50:22 +0900

    fix:divタグの削除

--- src/app/component/Atoms/EditButton/index.tsx
+++ src/app/component/Atoms/EditButton/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import Pencil from '../../../../../public/pencil.svg';
 
 const EditButton = () => {
-  return (
-    <div className='w-5 h-5 flex justify-center items-center'>
-      <Pencil className='hover:bg-[#B8B8B8]/20 rounded-full' />
-    </div>
-  );
+  return <Pencil className='hover:bg-[#B8B8B8]/20 rounded-full' />;
 };
 
 export default EditButton;
