1308820e022b8067ad1ad69e75f2e7cf5fa494d8
immunity428 <pega921sus@gmail.com>
2025-05-05 16:04:20 +0900

    途中経過

--- src/app/component/Atoms/Checkbox/index.tsx
+++ src/app/component/Atoms/Checkbox/index.tsx
@@ -1,7 +1,12 @@
+'use client';
 import React from 'react';
 import Check from '../../../../../public/check.svg';
 
-const Checkbox = () => {
+type CheckboxProps = {
+  onClick: () => void;
+};
+
+const Checkbox: React.FC<CheckboxProps> = ({ onClick }) => {
   return (
     <div className='w-5 h-5 border-[2px] border-[#B8B8B8] flex items-center justify-center group rounded-[2px]'>
       <Check className='hidden group-hover:block' />
